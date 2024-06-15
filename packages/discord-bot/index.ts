a89e15d8013eb48c12240f417f115da3e3fc9fb9
Kaito Udagawa <umireon@gmail.com>
2024-06-15 17:17:39 +0900

    Update index.ts

--- packages/discord-bot/index.ts
+++ packages/discord-bot/index.ts
@@ -8,6 +8,8 @@ const port = Number(process.env["PORT"] ?? 3000);
 app.use(express.raw());
 
 app.get("/", async (req, res) => {
+  console.log(req.body);
+  console.log(req.body.toString("utf-8"));
   const isValidRequest = await verify(req, req.body);
   if (!isValidRequest) {
     res.status(401);
