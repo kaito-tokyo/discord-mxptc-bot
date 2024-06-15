b504cab9ea562c8e0117a737d123b692a8606551
Kaito Udagawa <umireon@gmail.com>
2024-06-15 17:50:53 +0900

    Format

--- packages/discord-bot/index.ts
+++ packages/discord-bot/index.ts
@@ -9,18 +9,20 @@ const port = Number(process.env["PORT"] ?? 3000);
 app.post(
   "/",
   express.raw({ type: "*/*" }),
-  verifyDiscordInteractionSigunature("e5bdebe3264718958fdc0fc10f15d124da3e4434b064a0568784db8d617f04ef"),
+  verifyDiscordInteractionSigunature(
+    "e5bdebe3264718958fdc0fc10f15d124da3e4434b064a0568784db8d617f04ef",
+  ),
   async (req, res) => {
     const message = JSON.parse(req.body.toString());
     if (message.type === InteractionType.APPLICATION_COMMAND) {
       res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-          content: "Hello World!"
-        }
+          content: "Hello World!",
+        },
       });
     }
-  }
+  },
 );
 
 app.listen(port, () => {
--- packages/discord-bot/validator.ts
+++ packages/discord-bot/validator.ts
@@ -2,7 +2,11 @@ import type { Request, Response, NextFunction } from "express";
 import { verifyKey } from "discord-interactions";
 
 export function verifyDiscordInteractionSigunature(clientPublicKey: string) {
-  return async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
+  return async (
+    req: Request,
+    _res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
     const signature = req.get("X-Signature-Ed25519");
     if (!signature) {
       next(new Error("No signature"));
@@ -11,16 +15,21 @@ export function verifyDiscordInteractionSigunature(clientPublicKey: string) {
 
     const timestamp = req.get("X-Signature-Timestamp");
     if (!timestamp) {
-      next(new Error("No timestamp"))
+      next(new Error("No timestamp"));
       return;
     }
 
-    const isValidRequest = await verifyKey(req.body, signature, timestamp, clientPublicKey);
+    const isValidRequest = await verifyKey(
+      req.body,
+      signature,
+      timestamp,
+      clientPublicKey,
+    );
     if (!isValidRequest) {
       next(new Error("Invalid request"));
       return;
     }
 
     next();
-  }
+  };
 }
