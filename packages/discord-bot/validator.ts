e52239575c631e78e423ff9c9fee25e5fe101be9
Kaito Udagawa <umireon@gmail.com>
2024-06-15 17:04:57 +0900

    Update validator.ts

--- packages/discord-bot/validator.ts
+++ packages/discord-bot/validator.ts
@@ -1,20 +1,22 @@
-import type { Request, Response } from 'express';
-import { verifyKey } from 'discord-interactions';
+import type { Request } from "express";
+import { verifyKey } from "discord-interactions";
 
-export async function verify(
-  req: Request,
-  buf: Buffer
-): Promise<Boolean> {
-  const signature = req.get('X-Signature-Ed25519');
+export async function verify(req: Request, buf: Buffer): Promise<Boolean> {
+  const signature = req.get("X-Signature-Ed25519");
   if (!signature) {
     return false;
   }
 
-  const timestamp = req.get('X-Signature-Timestamp');
+  const timestamp = req.get("X-Signature-Timestamp");
   if (!timestamp) {
-    return false
+    return false;
   }
 
-  const isValidRequest = await verifyKey(buf, signature, timestamp, 'e5bdebe3264718958fdc0fc10f15d124da3e4434b064a0568784db8d617f04ef');
+  const isValidRequest = await verifyKey(
+    buf,
+    signature,
+    timestamp,
+    "e5bdebe3264718958fdc0fc10f15d124da3e4434b064a0568784db8d617f04ef",
+  );
   return isValidRequest;
 }
