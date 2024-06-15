fa8899db60ce242a8c826c099657193d41a14693
Kaito Udagawa <umireon@gmail.com>
2024-06-15 17:48:37 +0900

    Fix

--- packages/discord-bot/index.ts
+++ packages/discord-bot/index.ts
@@ -1,26 +1,27 @@
-import { InteractionResponseType } from "discord-interactions";
+import { InteractionResponseType, InteractionType } from "discord-interactions";
 import express from "express";
 
-import { verify } from "./validator.js";
+import { verifyDiscordInteractionSigunature } from "./validator.js";
 
 const app = express();
 const port = Number(process.env["PORT"] ?? 3000);
 
-app.post("/", express.raw(), async (req, res) => {
-  const isValidRequest = await verify(req, req.body);
-  if (!isValidRequest) {
-    res.status(401);
-    res.send("Bad sigunature!");
-    return;
-  }
-
-  res.send({
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-    data: {
-      content: "Hello World!"
+app.post(
+  "/",
+  express.raw({ type: "*/*" }),
+  verifyDiscordInteractionSigunature("e5bdebe3264718958fdc0fc10f15d124da3e4434b064a0568784db8d617f04ef"),
+  async (req, res) => {
+    const message = JSON.parse(req.body.toString());
+    if (message.type === InteractionType.APPLICATION_COMMAND) {
+      res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: "Hello World!"
+        }
+      });
     }
-  });
-});
+  }
+);
 
 app.listen(port, () => {
   console.log(`Listing on port ${port}`);
--- packages/discord-bot/validator.ts
+++ packages/discord-bot/validator.ts
@@ -1,22 +1,26 @@
-import type { Request } from "express";
+import type { Request, Response, NextFunction } from "express";
 import { verifyKey } from "discord-interactions";
 
-export async function verify(req: Request, buf: Buffer): Promise<Boolean> {
-  const signature = req.get("X-Signature-Ed25519");
-  if (!signature) {
-    return false;
-  }
+export function verifyDiscordInteractionSigunature(clientPublicKey: string) {
+  return async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
+    const signature = req.get("X-Signature-Ed25519");
+    if (!signature) {
+      next(new Error("No signature"));
+      return;
+    }
 
-  const timestamp = req.get("X-Signature-Timestamp");
-  if (!timestamp) {
-    return false;
-  }
+    const timestamp = req.get("X-Signature-Timestamp");
+    if (!timestamp) {
+      next(new Error("No timestamp"))
+      return;
+    }
 
-  const isValidRequest = await verifyKey(
-    buf,
-    signature,
-    timestamp,
-    "e5bdebe3264718958fdc0fc10f15d124da3e4434b064a0568784db8d617f04ef",
-  );
-  return isValidRequest;
+    const isValidRequest = await verifyKey(req.body, signature, timestamp, clientPublicKey);
+    if (!isValidRequest) {
+      next(new Error("Invalid request"));
+      return;
+    }
+
+    next();
+  }
 }
