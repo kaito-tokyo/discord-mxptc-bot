e1ed64f78501014531fc3fc25873a93dab3b053c
Kaito Udagawa <umireon@gmail.com>
2024-06-15 18:35:41 +0900

    Fix

--- packages/discord-bot/index.ts
+++ packages/discord-bot/index.ts
@@ -1,3 +1,4 @@
+import { APIApplicationCommandInteraction } from "discord-api-types/v10";
 import {
   InteractionResponseType,
   InteractionType,
@@ -8,20 +9,31 @@ import express from "express";
 const app = express();
 const port = Number(process.env["PORT"] ?? 3000);
 
+function isApplicationCommandInteraction(
+  message: unknown,
+): message is APIApplicationCommandInteraction {
+  if (!message || typeof message !== "object" || !("type" in message)) {
+    return false;
+  }
+  return message.type === InteractionType.APPLICATION_COMMAND;
+}
+
 app.post(
   "/",
   verifyKeyMiddleware(
     "e5bdebe3264718958fdc0fc10f15d124da3e4434b064a0568784db8d617f04ef",
   ),
   async (req, res) => {
-    const message = JSON.parse(req.body.toString());
-    if (message.type === InteractionType.APPLICATION_COMMAND) {
-      res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: "Hello World!",
-        },
-      });
+    const message: unknown = JSON.parse(req.body.toString());
+    if (isApplicationCommandInteraction(message)) {
+      if (message.data.name === "ping") {
+        res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: "Hello World!",
+          },
+        });
+      }
     }
   },
 );
