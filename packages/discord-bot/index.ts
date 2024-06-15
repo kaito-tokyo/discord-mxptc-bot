7263e44745508afaf6884e19fa6f47fe4be25373
Kaito Udagawa <umireon@gmail.com>
2024-06-15 17:03:40 +0900

    Fix

--- packages/discord-bot/index.ts
+++ packages/discord-bot/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
 
+import { verify } from "./validator.js";
+
 const app = express();
 const port = Number(process.env["PORT"] ?? 3000);
 
-app.get("/", (_, res) => {
+app.use(express.raw());
+
+app.get("/", async (req, res) => {
+  const isValidRequest = await verify(req, req.body);
+  if (isValidRequest) {
+    res.status(401);
+    res.send("Bad sigunature!");
+    return;
+  }
+
   res.send("Hello World!");
 });
 
---
+++ packages/discord-bot/validator.ts
@@ -0,0 +1,20 @@
+import type { Request, Response } from 'express';
+import { verifyKey } from 'discord-interactions';
+
+export async function verify(
+  req: Request,
+  buf: Buffer
+): Promise<Boolean> {
+  const signature = req.get('X-Signature-Ed25519');
+  if (!signature) {
+    return false;
+  }
+
+  const timestamp = req.get('X-Signature-Timestamp');
+  if (!timestamp) {
+    return false
+  }
+
+  const isValidRequest = await verifyKey(buf, signature, timestamp, 'e5bdebe3264718958fdc0fc10f15d124da3e4434b064a0568784db8d617f04ef');
+  return isValidRequest;
+}
