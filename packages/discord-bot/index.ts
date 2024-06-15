c0b069d29db87d33865efffb4014d519e267d238
Kaito Udagawa <umireon@gmail.com>
2024-06-15 17:56:33 +0900

    Update index.ts

--- packages/discord-bot/index.ts
+++ packages/discord-bot/index.ts
@@ -1,13 +1,18 @@
-import { InteractionResponseType, InteractionType, verifyKeyMiddleware } from "discord-interactions";
+import {
+  InteractionResponseType,
+  InteractionType,
+  verifyKeyMiddleware,
+} from "discord-interactions";
 import express from "express";
-
 
 const app = express();
 const port = Number(process.env["PORT"] ?? 3000);
 
 app.post(
   "/",
-  verifyKeyMiddleware("e5bdebe3264718958fdc0fc10f15d124da3e4434b064a0568784db8d617f04ef"),
+  verifyKeyMiddleware(
+    "e5bdebe3264718958fdc0fc10f15d124da3e4434b064a0568784db8d617f04ef",
+  ),
   async (req, res) => {
     const message = JSON.parse(req.body.toString());
     if (message.type === InteractionType.APPLICATION_COMMAND) {
