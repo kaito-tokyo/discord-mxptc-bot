73a7190e1ed7f1504af8b58ec06a8d24759d63f7
Kaito Udagawa <umireon@gmail.com>
2024-06-15 19:56:14 +0900

    Fix

--- packages/discord-bot/index.ts
+++ packages/discord-bot/index.ts
@@ -8,6 +8,7 @@ import express from "express";
 
 import { loadDiscordPublicKey } from "./constants.js";
 import { registerGlobalCommands } from "./registerGlobalCommands.js";
+import * as match from "./commands/match.js";
 
 function isApplicationCommandInteraction(
   message: unknown,
@@ -26,13 +27,8 @@ const port = Number(process.env["PORT"] ?? 3000);
 app.post("/", verifyKeyMiddleware(loadDiscordPublicKey()), async (req, res) => {
   const message: unknown = JSON.parse(req.body.toString());
   if (isApplicationCommandInteraction(message)) {
-    if (message.data.name === "ping") {
-      res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: "Hello World!",
-        },
-      });
+    if (message.data.name === match.data.name) {
+      await match.execute(message, res);
     }
   }
 });
--- packages/discord-bot/registerGlobalCommands.ts
+++ packages/discord-bot/registerGlobalCommands.ts
@@ -2,7 +2,7 @@ import { REST } from "@discordjs/rest";
 import { loadDiscordApplicationId, loadDiscordBotToken } from "./constants.js";
 import { Routes } from "discord-api-types/v10";
 
-import * as registerMatch from "./commands/registerMatch.js";
+import * as match from "./commands/match.js";
 
 export async function registerGlobalCommands() {
   const discordApplicationId = loadDiscordApplicationId();
@@ -10,6 +10,6 @@ export async function registerGlobalCommands() {
 
   const rest = new REST({ version: "10" }).setToken(discordBotToken);
   await rest.put(Routes.applicationCommands(discordApplicationId), {
-    body: [registerMatch.data.toJSON()],
+    body: [match.data.toJSON()],
   });
 }
